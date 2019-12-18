e896ba47c9d6739db499ff2a0c5aefa73dbb4a5a
Ed Courage <edcourage@live.com>
2019-12-18 19:01:04 +0000

    more comments added

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,24 +11,31 @@ class GildedRose
     # Iterates through loop
     @items.each do |item|
 
-      # First if statement (has nested statements)
+      # Deals with quility before sell by date
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
+          # removes quality
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
+      # Aged Brie and Backstage Passes
       else
         if item.quality < 50
+          # adds quality
           item.quality = item.quality + 1
+          # Backstage Passes for remainder of if statement
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
+                # adds quality
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
+              # stops quality going over 50
               if item.quality < 50
+                # adds quality
                 item.quality = item.quality + 1
               end
             end
@@ -36,12 +43,12 @@ class GildedRose
         end
       end
 
-      # Second if statement
+      # Removes sell in day from all items apart from Sulfuras, Hand of Ragnaros
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
 
-      # Third if statement (has nested statements)
+      # Deals with quility after sell by date
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -50,9 +57,11 @@ class GildedRose
                 item.quality = item.quality - 1
               end
             end
+          #sets Backstage pass quality to 0 after sell by date (gig)
           else
             item.quality = item.quality - item.quality
           end
+        # stops quality going over 50
         else
           if item.quality < 50
             item.quality = item.quality + 1
